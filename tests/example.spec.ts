ff1e3bb0fe742ec2c1cfe8c72a38c4fa7c421378
Ana-Hanny <ana@calc-i.com>
2024-11-11 16:24:32 +0000

    test title edit

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,5 +4,5 @@ test('has title', async ({ page }) => {
   await page.goto('http://localhost:8000');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Practice change/);
+  await expect(page).toHaveTitle(/Page practice/);
 });
